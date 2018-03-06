1434c96f1574256f64d52ac1d7cffebb734a2903
Paul Chiu <paulchiu@gmail.com>
2018-03-07 07:19:07 +1000

    Got get products working.

--- app/index.js
+++ app/index.js
@@ -11,8 +11,18 @@ require('yargs')
             .positional('password', {
                 describe: 'Password to sign in with',
             })
+            .option('limit', {
+                describe: 'Max number of results to fetch',
+                default: 10000,
+                number: true,
+            })
+            .option('page', {
+                describe: 'TradeGecko page to start getting results from',
+                default: 1,
+                number: true,
+            })
     }, (argv) => {
-        chrome.listVariants(argv.username, argv.password);
+        chrome.listVariants(argv.username, argv.password, argv.page, argv.limit);
     })
     .help()
     .argv
