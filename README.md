a945e0945098e5ba5a8836655e3737d0c40e84d6
paulchiu <paulchiu@gmail.com>
2018-03-14 03:28:31 +1000

    Feature/headless chrome (#3)
    * Added puppetier.
    * Created basic command to sign in.
    * Got product retrival working.
    * Got get products working.
    * Completed get resources command.
    * Added and used prettier.
    * Got publish variant working.
    * Got create resource working.
    * Implemented general fetch command.
    * Got cli templates working.
    * Got batch fetch kind of working.
    * Got bulk fetch working.
    * Created response logger.
    * Made fetch also use progress bar.
    * Improved formatting.
    * Updated packages json and updated readme.

--- README.md
+++ README.md
@@ -1,6 +1,111 @@
-# TradeGecko Headless
+# TradeGecko Headless CLI
 
-Headless TradeGecko interaction CLI for those without API access.
+Headless [TradeGecko](https://www.tradegecko.com/) interaction CLI for those without API access or for automating calls not yet published in the [TradeGecko API Reference](http://developer.tradegecko.com/).
+
+# Install
+
+```
+npm install --global tradegecko-headless
+```
+
+# Usage
+
+The following usage examples should be read sequentially as it references results from previous commands.
+
+It is also recommended that all result files be viewed or manipulated with [jq](https://stedolan.github.io/jq/).
+
+## Command pattern
+
+To use the CLI, identify the command you want to execute and provide any parameters and options.
+
+```
+tgcli [command] [options]
+```
+
+To see the full list of commands, execute
+
+```
+tgcli --help
+```
+
+## Set username and password
+
+Username and passowrd can be provided as environment variables or as parameters. It is
+recommended that environment variables be used.
+
+```
+export TG_USERNAME="foo@example.com"
+export TG_PASSWORD="bar"
+```
+
+## Download a list of resources
+
+Download 2nd and 3rd oldest `variants` resource, keeping only fields `id` and `created_at` and save them to file `variants.json`
+
+```
+tgcli tg:resource-list:download variants variants.json --limit=2 --offset=1 --fields=id,created_at
+```
+
+### Sample output
+
+```
+Signing in ...
+Getting variants count ...
+Getting variants ...
+ ████████████████████████████████████████ 100% | ETA: 0s | 2/2
+Saving variants to file ...
+Done
+```
+
+### Sample output file contents
+
+```
+[{"id":1,"created_at":"2017-01-11T11:00:00.000Z"},{"id":2,"created_at":"2017-01-11T12:00:00.000Z"}]
+```
+
+## Perform fetch actions on a JSON object list
+
+Make a fetch call using `variants.json` and output results to `fetched-variants.json`. The fetch
+call should make a GET call to get the variant by id.
+
+```
+tgcli tg:resource-list:fetch variants.json fetched-variants.json GET "variants/{{id}}"
+```
+
+### Sample output
+
+```
+Signing in ...                                                                                                                                                                                                     
+Performing fetches ...                                                                                                                                                                                             
+ ████████████████████████████████████████ 100% | ETA: 0s | 2/2                  
+Saving responses to file ...                                                                                
+Done
+```
+
+### Sample output file contents
+
+Plese note the following outputs have been shortended for readability.
+
+```
+[{"variant":{"id":1,"created_at":"2017-01-11T11:00:00.000Z","updated_at":"...","committed_stock_levels":{}}},{"variant":{"id":2,"created_at":"2017-01-11T12:00:00.000Z","updated_at":"...","committed_stock_levels":{}}}]
+```
+
+# Commands
+
+For a full list of commands, execute the program with `--help`. All commands should be prefixed with `tgcli`.
+
+|Command|Description|
+|-------|-----------|
+|`tg:resource-list:download [resource] [file]`|Download a list of a given resource|
+|`tg:resource-list:fetch [listFile] [responsesFile] [method] [endpoint]`|Perform fetch action on each record of a provided resource list JSON file|
+|`tg:ajax:fetch [method] [endpoint]`|Perform a fetch call against the TradeGecko AJAX API, see the [MDN entry for Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API) for details|                                
+# Known TradeGecko resources
+
+The following list of resources were obtained by observing Chrome debugger XHR activity for connections related to `https://go.tradegecko.com/ajax`
+
+- channels
+- products
+- variants
 
 # Built with
 
---
+++ app/index.js
@@ -0,0 +1,145 @@
+#!/usr/bin/env node
+
+const commands = require("../lib/commands");
+
+require("yargs")
+  .env("TG")
+  .usage(
+    "$0 [command] [options]" +
+      "\n" +
+      "\nIt is recommended that username and password be set as environment variables of" +
+      "\nTG_USERNAME and TG_PASSWORD, respectively."
+  )
+  .option("username", {
+    describe: "Username/email to sign in with",
+    demandOption: true
+  })
+  .option("password", {
+    describe: "Password to sign in with",
+    demandOption: true
+  })
+  .option("verbosity", {
+    alias: "v",
+    describe: "Choose different levels of verbosity",
+    choices: ["v", "vv"]
+  })
+  .command(
+    "tg:resource-list:download [resource] [file]",
+    "Download a list of a given resource",
+    yargs => {
+      yargs
+        .positional("resource", {
+          describe:
+            "AJAX resource available in TradeGecko; should confirm existance by seeing XHR" +
+            " requests in normal session that match the request pattern" +
+            " https://go.tradegecko.com/ajax/%s" +
+            " \n\nExample of valid resources include: products, variants",
+          type: "string"
+        })
+        .positional("file", {
+          describe: "File to sasve resources to; output will be in JSON",
+          type: "string"
+        })
+        .option("offset", {
+          describe: "Results to offset",
+          default: 0,
+          number: true
+        })
+        .option("limit", {
+          describe: "Max number of results to fetch",
+          default: 10000,
+          number: true
+        })
+        .option("fields", {
+          describe:
+            "Fields to return in listed resources as CSV for example: id,created_at"
+        })
+        .coerce("fields", f => f.split(",").map(s => s.trim()))
+        .example(
+          "tg:resource:list products products.json --limit=1 -v=vv --fields=id,created_at",
+          "Download id and created at fields for one product"
+        );
+    },
+    commands.downloadResourceList
+  )
+  .command(
+    "tg:resource-list:fetch [listFile] [responsesFile] [method] [endpoint]",
+    "Perform fetch action on each record of a provided resource list JSON file",
+    yargs => {
+      yargs
+        .positional("listFile", {
+          describe:
+            "Resources list file. This should be a JSON file like one created by tg:resource:list",
+          type: "string"
+        })
+        .positional("responsesFile", {
+          describe:
+            "Fetch responses file. This will be another list file that may or may not be resources.",
+          type: "string"
+        })
+        .positional("method", {
+          describe: 'Request method, such as "POST"',
+          type: "string"
+        })
+        .positional("endpoint", {
+          describe:
+            "Mustashe template string representing the endpoint. Template data will be a" +
+            "record from the resources file provided.",
+          type: "string"
+        })
+        .option("body", {
+          describe:
+            "Mustashe template string representing the fetch body. Template data will be a" +
+            "record from the resources file provided. If provided it should be a JSON string.",
+          string: true
+        })
+        .option("offset", {
+          describe: "Number of variants to skip",
+          default: 0,
+          number: true
+        })
+        .option("limit", {
+          describe: "Max number of variants to publish",
+          default: 10000,
+          number: true
+        })
+        .example(
+          'tg:resource-list:fetch variants.json channels.json POST "variants/{{id}}/channels" --body=\'{"channel":{"channel_id":123}}\'',
+          "Publish all variants in the provided file to channel 123"
+        );
+    },
+    commands.fetchWithResourceList
+  )
+  .command(
+    "tg:ajax:fetch [method] [endpoint]",
+    "Perform a fetch call against the TradeGecko AJAX API, see MDN entry for Fetch API" +
+      " for details",
+    yargs => {
+      yargs
+        .positional("method", {
+          describe: 'Request method, such as "POST"',
+          type: "string"
+        })
+        .positional("endpoint", {
+          describe:
+            'Resource endpoint, such a "products", please note that all endpoints' +
+            " automatically have https://go.tradegecko.com/ajax/ prefixed",
+          type: "string"
+        })
+        .option("body", {
+          describe:
+            "Body to be provided to the fetch call, if provided should be a JSON string",
+          string: true
+        })
+        .example(
+          "tg:ajax:fetch GET 'products?limit=1&page=1'",
+          "Get the first product in the user's inventory"
+        )
+        .example(
+          "tg:ajax:fetch POST 'variants/123/channels' --body='{\"channel\":{\"channel_id\":321}}'",
+          "Publish variant 123 on channel 321"
+        );
+    },
+    commands.fetch
+  )
+  .help().argv;
